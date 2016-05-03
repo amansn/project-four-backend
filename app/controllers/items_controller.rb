412c74502eeeadad9c9644925d1c80b2a7a6080d
Aman <amanjeet@gmail.com>
2016-05-03 19:34:22 -0400

    Added create

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,32 @@
 class ItemsController < ApplicationController
+  skip_before_filter :verify_authenticity_token
+
   def index
     @items = Item.all
     render json: @items, status: 200
+  end
+
+  def show
+    @item = Item.find(params[:id])
+    render json: @item
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      render status: 200, json: {
+        message: "Sucessfully added item to inventory."
+      }.to_json
+    else
+      render status: 500, json: {
+        errors: @item.errors
+      }.to_json
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :brand, :size, :color, :purchase_date, :purchased_from, :price, :taxes, :shipping_cost, :customs_fees, :total_cost, :image_url)
   end
 end
